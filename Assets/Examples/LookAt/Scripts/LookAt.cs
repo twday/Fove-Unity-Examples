8021364315285c60c57377673b0afd3e5205fee4
Thomas Day <t.day@chester.ac.uk>
2017-02-03 12:57:31 +0000

    Created LookAt Example

---
+++ Assets/Examples/LookAt/Scripts/LookAt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+    Collider mCollider;
+    Light mLight;
+
+	// Use this for initialization
+	void Start () {
+        mCollider = GetComponent<Collider>();
+        mLight = GetComponentInChildren<Light>();
+
+        if (mCollider == null)
+            mCollider = gameObject.AddComponent<BoxCollider>();
+        if (mLight == null) {
+            mLight = gameObject.AddComponent<Light>();
+            mLight.range = 5f;
+            mLight.intensity = 2f;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FoveInterface.IsLookingAtCollider(mCollider))
+        {
+            Debug.Log("COLLISION");
+            mLight.enabled = true;
+        } else
+        {
+            mLight.enabled = false;
+        }
+	}
+}
